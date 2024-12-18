268519a05b10e1c3c57c0439eaad7a06eeea4986
Ubuntu <ubuntu@ip-172-31-31-234.ap-southeast-2.compute.internal>
2024-12-18 07:25:27 +0000

    edited

--- main.tf
+++ main.tf
@@ -1,23 +1,24 @@
+# main.tf
+
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-1"  # Set the region to us-east-1
 }
 
-# S3 Bucket
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-terraform-s3-bucket-example"
-
-  tags = {
-    Name        = "MyBucket"
-    Environment = "Dev"
-  }
+  bucket = "my-terraform-bucket"  # Ensure the bucket name is globally unique
+  acl    = "private"  # Choose the appropriate access control (e.g., private)
 }
 
-# EC2 Instance
 resource "aws_instance" "my_instance" {
-  ami           = "ami-0e2c8caa4b6378d8c"
-  instance_type = "t2.micro"
+  ami           = "ami-0e2c8caa4b6378d8c"  # Replace with a valid AMI ID for us-east-1
+  instance_type = "t2.micro"  # Specify the EC2 instance type
 
   tags = {
-    Name = "MyEC2Instance"
+    Name = "MyTerraformInstance"
   }
 }
+
+output "instance_public_ip" {
+  value = aws_instance.my_instance.public_ip  # Output the EC2 instance's public IP
+}
+
